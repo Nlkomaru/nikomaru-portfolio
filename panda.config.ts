b635a27ec0f44f06cf24ff494745c4d6d92d2b48
nlkomaru <76208219+Nlkomaru@users.noreply.github.com>
2024-12-17 13:03:05 +0900

    :sparkles: Init commit

---
+++ panda.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from "@pandacss/dev";
+import { createPreset } from "@park-ui/panda-preset";
+import amber from "@park-ui/panda-preset/colors/amber";
+import sand from "@park-ui/panda-preset/colors/sand";
+
+export default defineConfig({
+    // Whether to use css reset
+    preflight: true,
+    presets: [
+        createPreset({ accentColor: amber, grayColor: sand, radius: "sm" }),
+    ],
+    // Where to look for your css declarations
+    include: [
+        "./src/components/**/*.{ts,tsx,js,jsx}",
+        "./src/app/**/*.{ts,tsx,js,jsx}",
+    ],
+    // Files to exclude
+    exclude: [],
+
+    // Useful for theme customization
+    theme: {
+        extend: {},
+    },
+    jsxFramework: "react",
+    // The output directory for your css system
+    outdir: "styled-system",
+});
