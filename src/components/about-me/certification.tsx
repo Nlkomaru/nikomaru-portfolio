54d33a84f423a0657b5aa8af94745bfce59bf56c
nlkomaru <76208219+Nlkomaru@users.noreply.github.com>
2025-01-21 14:04:49 +0900

    :sparkles: Add certification component mock

--- src/components/about-me/certification.tsx
+++ src/components/about-me/certification.tsx
@@ -1,3 +1,44 @@
-export default function Home() {
-    return <></>;
-}
+import dayjs from "dayjs";
+
+export const Certification = () => {
+    const cert: certification[] = [
+        {
+            issuedAt: dayjs("2019-07-01"),
+            expiresAt: null,
+            id: "world-heritage-3",
+            name: "世界遺産検定 3級",
+        },
+        {
+            issuedAt: dayjs("2022-06-27"),
+            expiresAt: null,
+            id: "fundamental-information-technology-engineer-examination",
+            name: "基本情報技術者試験",
+        },
+        {
+            issuedAt: dayjs("2023-06-29"),
+            expiresAt: null,
+            id: "applied-information-technology-engineer-examination",
+            name: "応用情報技術者試験",
+        },
+    ];
+    return (
+        <>
+            {cert
+                .filter((it) =>
+                    it.expiresAt ? it.expiresAt?.isAfter(dayjs()) : true,
+                )
+                .map((cert) => (
+                    <div key={cert.id}>
+                        {`${cert.issuedAt.format("YYYY-MM")} ${cert.name}`}
+                    </div>
+                ))}
+        </>
+    );
+};
+
+type certification = {
+    issuedAt: dayjs.Dayjs;
+    expiresAt: dayjs.Dayjs | null;
+    id: string;
+    name: string;
+};
